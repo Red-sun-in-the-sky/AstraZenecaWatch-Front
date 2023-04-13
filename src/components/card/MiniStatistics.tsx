8e22ee55359e035cb0a40c23e7e3078e4eb417d7
Feru <f1nandocor@gmail.com>
2023-04-12 22:50:09 -0600

    Main dashboard pt1

--- src/components/card/MiniStatistics.tsx
+++ src/components/card/MiniStatistics.tsx
@@ -1,55 +1,124 @@
 // Chakra imports
-import { Flex, Stat, StatLabel, StatNumber, useColorModeValue, Text } from '@chakra-ui/react';
+import {
+  Flex,
+  Stat,
+  StatLabel,
+  StatNumber,
+  useColorModeValue,
+  Text,
+} from "@chakra-ui/react";
 // Custom components
-import Card from 'components/card/Card';
+import Card from "components/card/Card";
+import { useState } from "react";
 
 export default function Default(props: {
-	startContent?: JSX.Element;
-	endContent?: JSX.Element;
-	name: string;
-	growth?: string | number;
-	value: string | number;
+  startContent?: JSX.Element;
+  endContent?: JSX.Element;
+  name?: string;
+  growth?: string | number;
+  value: string | number;
+  setData: any;
+  setShowData: any;
 }) {
-	const { startContent, endContent, name, growth, value } = props;
-	const textColor = useColorModeValue('secondaryGray.900', 'white');
-	const textColorSecondary = 'secondaryGray.600';
+  const {
+    startContent,
+    endContent,
+    name,
+    growth,
+    value,
+    setData,
+    setShowData,
+  } = props;
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const textColorSecondary = "secondaryGray.600";
 
-	return (
-		<Card py='15px'>
-			<Flex my='auto' h='100%' align={{ base: 'center', xl: 'start' }} justify={{ base: 'center', xl: 'center' }}>
-				{startContent}
+  const [isHovering, setHovering] = useState(false);
 
-				<Stat my='auto' ms={startContent ? '18px' : '0px'}>
-					<StatLabel
-						lineHeight='100%'
-						color={textColorSecondary}
-						fontSize={{
-							base: 'sm'
-						}}>
-						{name}
-					</StatLabel>
-					<StatNumber
-						color={textColor}
-						fontSize={{
-							base: '2xl'
-						}}>
-						{value}
-					</StatNumber>
-					{growth ? (
-						<Flex align='center'>
-							<Text color='green.500' fontSize='xs' fontWeight='700' me='5px'>
-								{growth}
-							</Text>
-							<Text color='secondaryGray.600' fontSize='xs' fontWeight='400'>
-								since last month
-							</Text>
-						</Flex>
-					) : null}
-				</Stat>
-				<Flex ms='auto' w='max-content'>
-					{endContent}
-				</Flex>
-			</Flex>
-		</Card>
-	);
+  const handleMouseEnter = () => setHovering(true);
+  const handleMouseLeave = () => setHovering(false);
+  const handleMouseDown = () => {
+    setData({
+      name: value,
+      services: [
+        {
+          name: value + " Critical Service 1",
+          status: "green",
+        },
+        {
+          name: value + " Critical Service 2",
+          status: "yellow",
+        },
+        {
+          name: value + " Critical Service 3",
+          status: "green",
+        },
+        {
+          name: value + " Critical Service 4",
+          status: "green",
+        },
+        {
+          name: value + " Critical Service 5",
+          status: "red",
+        },
+      ],
+    });
+    setShowData(true);
+  };
+
+  return (
+    <Card
+      py="15px"
+      style={{
+        cursor: isHovering ? "pointer" : "default",
+        top: isHovering ? "-2px" : "",
+        border: isHovering ? ".5px grey solid" : "",
+        transition: isHovering ? "top ease 0.5s" : "",
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onMouseDown={handleMouseDown}
+    >
+      <Flex
+        my="auto"
+        h="100%"
+        align={{ base: "center", xl: "start" }}
+        justify={{ base: "center", xl: "center" }}
+      >
+        {startContent}
+
+        <Stat my="auto" ms={startContent ? "18px" : "0px"}>
+          <StatLabel
+            lineHeight="100%"
+            color={textColorSecondary}
+            fontSize={{
+              base: "sm",
+            }}
+          >
+            {name}
+          </StatLabel>
+          <StatNumber
+            color={textColor}
+            fontSize={{
+              base: "2xl",
+            }}
+          >
+            {value}
+          </StatNumber>
+          {growth ? (
+            <Flex align="center">
+              <Text color="green.500" fontSize="xs" fontWeight="700" me="5px">
+                {growth}
+              </Text>
+              <Text color="secondaryGray.600" fontSize="xs" fontWeight="400">
+                since last month
+              </Text>
+            </Flex>
+          ) : null}
+        </Stat>
+        <Flex ms="auto" w="max-content">
+          {endContent}
+        </Flex>
+      </Flex>
+    </Card>
+  );
 }
