57d7d1b33b09ca692eb5530e3f3c463128c05416
David Berrospe Ramírez <david.berrospe.r03@gmail.com>
2023-04-13 00:56:54 -0600

    Added favorites section.

---
+++ src/views/admin/favorites/components/RightPanel.tsx
@@ -0,0 +1,96 @@
+import {
+  CloseButton,
+  Divider,
+  Flex,
+  Heading,
+  Text,
+  HStack,
+  Icon,
+  List,
+  ListItem,
+  VStack,
+} from "@chakra-ui/react";
+import Card from "components/card/Card";
+import { useEffect, useState } from "react";
+import {
+  MdCancel,
+  MdCheckCircle,
+  MdRemoveCircle,
+} from "react-icons/md";
+
+export const RightPanel = (props: {
+  data: any;
+  setShowData: any;
+  brandColor: any;
+  renderData?: any;
+}) => {
+  const { brandColor, setShowData, data } = props;
+  const [renderData, setRenderData] = useState([<></>]);
+
+  // Render BTG services
+  useEffect(() => {
+    const { services } = data;
+    const render = services.map((service: any, i: any) => {
+      let color = "green.500";
+      let text = "Service available";
+      let icon = MdCheckCircle;
+
+      if (service.status === "yellow") {
+        color = "yellow.400";
+        text = "Service disruption";
+        icon = MdRemoveCircle;
+      }
+      if (service.status === "red") {
+        color = "red.500";
+        text = "Service outage";
+        icon = MdCancel;
+      }
+
+      return (
+        <ListItem key={i} borderBottom=".1px solid #dadada" my={6}>
+          <Flex justify="space-between">
+            <VStack align="start">
+              <Heading size="sm">{service.name}</Heading>
+              <Heading size="sm" color={color}>
+                {text}
+              </Heading>
+            </VStack>
+            <HStack spacing={10}>
+              <Icon as={icon} color={color} fontSize={22} />
+            </HStack>
+          </Flex>
+        </ListItem>
+      );
+    });
+
+    setRenderData(render);
+  }, [data]);
+
+  return (
+    <Card>
+      <Flex justify="space-between" align="center">
+        <Heading
+          fontSize={{
+            base: "2xl",
+          }}
+        >
+          {data.name}
+        </Heading>
+        <CloseButton
+          w="30px"
+          h="30px"
+          color={brandColor}
+          onClick={() => {
+            setShowData(false);
+          }}
+        />
+      </Flex>
+      <Divider mt={4} mb={8} />
+      {data.type === "btg" ? (
+        <List spacing={3}>{renderData}</List>
+      ) : (
+        <Text>Pepepe</Text>
+      )}
+    </Card>
+  );
+};
