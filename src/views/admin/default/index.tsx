512b862fedef203007bebc2757359a7cd1948d40
Feru <f1nandocor@gmail.com>
2023-04-12 22:50:09 -0600

    Main dashboard pt1

--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -1,37 +1,186 @@
 import {
-  Avatar,
   Box,
   Flex,
-  FormLabel,
+  Grid,
+  GridItem,
   Icon,
-  Select,
   SimpleGrid,
   useColorModeValue,
+  Heading,
+  CloseButton,
+  Text,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  VStack,
+  HStack,
+  Tag,
+  TagLabel,
 } from "@chakra-ui/react";
-// Assets
-import Usa from "assets/img/dashboards/usa.png";
-// Custom components
-import MiniCalendar from "components/calendar/MiniCalendar";
+import Card from "components/card/Card";
+import { BTGComponent } from "./btg";
+import { TicketComponent } from "./ticket";
 import MiniStatistics from "components/card/MiniStatistics";
 import IconBox from "components/icons/IconBox";
+import { useEffect, useState } from "react";
 import {
-  MdAddTask,
-  MdAttachMoney,
-  MdBarChart,
-  MdFileCopy,
+  MdCancel,
+  MdCheckCircle,
+  MdClose,
+  MdMinimize,
+  MdRemoveCircle,
+  MdStar,
+  MdStarBorder,
 } from "react-icons/md";
-import ComplexTable from "views/admin/default/components/ComplexTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
-import Tasks from "views/admin/default/components/Tasks";
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
-import tableDataCheck from "views/admin/default/variables/tableDataCheck";
-import tableDataComplex from "views/admin/default/variables/tableDataComplex";
 
 export default function UserReports() {
   // Chakra Color Mode
-  const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
-  return <Box pt={{ base: "130px", md: "80px", xl: "80px" }}></Box>;
+  const brandColor = useColorModeValue("brand.500", "white");
+
+  const [showData, setShowData] = useState(false);
+  const [data, setData] = useState({ name: "", services: [] });
+  const [renderData, setRenderData] = useState([<></>]);
+
+  const [selected, setselected] = useState("btg");
+
+  useEffect(() => {
+    const { services } = data;
+    const render = services.map((service, i) => {
+      let color = "green.500";
+      let text = "Service available";
+      let icon = MdCheckCircle;
+      let star = MdStar;
+
+      if (i % 2 === 0) {
+        star = MdStarBorder;
+      }
+
+      if (service.status === "yellow") {
+        color = "yellow.400";
+        text = "Service disruption";
+        icon = MdRemoveCircle;
+      }
+      if (service.status === "red") {
+        color = "red.500";
+        text = "Service outage";
+        icon = MdCancel;
+      }
+
+      return (
+        <ListItem key={i} borderBottom=".1px solid #dadada" my={6}>
+          <Flex justify="space-between">
+            <VStack align="start">
+              <Heading size="sm">{service.name}</Heading>
+              <Heading size="sm" color={color}>
+                {text}
+              </Heading>
+            </VStack>
+            <HStack spacing={10}>
+              <Icon as={icon} color={color} fontSize={22} />
+              <Icon
+                as={star}
+                fontSize={20}
+                onMouseEnter={handleMouseEnter}
+                color="yellow.600"
+                style={{
+                  cursor: isHovering ? "pointer" : "default",
+                  transition: isHovering ? "top ease 0.5s" : "",
+                }}
+              />
+            </HStack>
+          </Flex>
+        </ListItem>
+      );
+    });
+
+    setRenderData(render);
+  }, [data]);
+
+  const [isHovering, setHovering] = useState(false);
+
+  const handleMouseEnter = () => setHovering(true);
+
+  return (
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <Grid gap={10} templateColumns="repeat(2, 1fr)">
+        <GridItem w="100%" h="10" colSpan={1}>
+          <HStack spacing={4} mb={4}>
+            <Tag
+              size="lg"
+              key="btg"
+              borderRadius="full"
+              variant="outline"
+              colorScheme="blue"
+              style={{
+                cursor: isHovering ? "pointer" : "default",
+                transition: isHovering ? "top ease 0.5s" : "",
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseDown={() => setselected("btg")}
+            >
+              <TagLabel>Technology groups</TagLabel>
+            </Tag>
+            <Tag
+              size="lg"
+              key="ticket"
+              borderRadius="full"
+              variant="outline"
+              colorScheme="blue"
+              style={{
+                cursor: isHovering ? "pointer" : "default",
+                transition: isHovering ? "top ease 0.5s" : "",
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseDown={() => setselected("ticket")}
+            >
+              <TagLabel>Incidents</TagLabel>
+            </Tag>
+          </HStack>
+          {selected === "btg" ? (
+            <BTGComponent
+              boxBg={boxBg}
+              brandColor={brandColor}
+              setData={setData}
+              setShowData={setShowData}
+            />
+          ) : (
+            <TicketComponent
+              boxBg={boxBg}
+              brandColor={brandColor}
+              setData={setData}
+              setShowData={setShowData}
+            />
+          )}
+        </GridItem>
+        {/* Right section */}
+        {showData && (
+          <GridItem w="100%" h="90%" colSpan={1}>
+            <Card>
+              <Flex justify="space-between" align="center">
+                <Heading
+                  fontSize={{
+                    base: "2xl",
+                  }}
+                >
+                  {data.name}
+                </Heading>
+                <CloseButton
+                  w="30px"
+                  h="30px"
+                  color={brandColor}
+                  onClick={() => {
+                    setShowData(false);
+                  }}
+                />
+              </Flex>
+              <Divider mt={4} mb={8} />
+              <List spacing={3}>{renderData}</List>
+            </Card>
+          </GridItem>
+        )}
+      </Grid>
+    </Box>
+  );
 }
---
+++ src/views/admin/default/ticket.tsx
@@ -0,0 +1,100 @@
+import {
+  HStack,
+  Icon,
+  SimpleGrid,
+  Tag,
+  TagLabel,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import MiniStatistics from "components/card/MiniStatistics";
+import IconBox from "components/icons/IconBox";
+import {
+  MdComputer,
+  MdEmojiPeople,
+  MdMail,
+  MdOutlineAttachMoney,
+  MdSearch,
+} from "react-icons/md";
+
+export const TicketComponent = (props: {
+  boxBg: any;
+  brandColor: any;
+  setData: any;
+  setShowData: any;
+}) => {
+  const { boxBg, brandColor, setData, setShowData } = props;
+
+  return (
+    <>
+      <SimpleGrid
+        columns={{ base: 1, md: 1, lg: 1, "2xl": 2 }}
+        gap="20px"
+        mb="20px"
+      >
+        <MiniStatistics
+          setData={setData}
+          setShowData={setShowData}
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon w="32px" h="32px" as={MdEmojiPeople} color={brandColor} />
+              }
+            />
+          }
+          value="Human Resources"
+        />
+        <MiniStatistics
+          setData={setData}
+          setShowData={setShowData}
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon
+                  w="32px"
+                  h="32px"
+                  as={MdOutlineAttachMoney}
+                  color={brandColor}
+                />
+              }
+            />
+          }
+          value="Finance"
+        />
+        <MiniStatistics
+          setData={setData}
+          setShowData={setShowData}
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={
+                <Icon w="32px" h="32px" as={MdComputer} color={brandColor} />
+              }
+            />
+          }
+          value="Development"
+        />
+        <MiniStatistics
+          setData={setData}
+          setShowData={setShowData}
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdSearch} color={brandColor} />}
+            />
+          }
+          value="Research"
+        />
+      </SimpleGrid>
+    </>
+  );
+};
